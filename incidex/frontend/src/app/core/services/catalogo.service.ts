ae3c76fb4fc34d847e8856319c1d55d6bce07dff
David Quintanilla <dquintanilla-2022275@kinal.edu.gt>
2026-09-16 10:17:11 -0600

    Feat:Se modifico el service de catalogo de la carpeta core del frontend

--- incidex/frontend/src/app/core/services/catalogo.service.ts
+++ incidex/frontend/src/app/core/services/catalogo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { RespuestaApi } from '../../models/api-response.model';
-import { Categoria, Departamento, Prioridad, Ubicacion } from '../../models/catalogo.model';
+import { Categoria, Departamento, Prioridad, Ubicacion } from '../../models/catalogo.models';
 
 const API_URL = 'http://localhost:3000/api';
 
